cc4a8a19a64a9bbdbc11463685606f33f7550a81
ElminaIusifova <32398063+ElminaIusifova@users.noreply.github.com>
2020-05-30 23:10:09 -0400

    using chromedriver

--- 01-facebook-login/main.py
+++ 01-facebook-login/main.py
@@ -1,15 +1,13 @@
 from selenium import webdriver
 from time import sleep
 
-browser_driver = webdriver.Chrome(executable_path='/Users/elminaiusifova/Desktop/Python/week6-python-selenium-task-ElminaIusifova/chromedriver')
-
+# browser_driver = webdriver.Chrome(executable_path='/Users/elminaiusifova/Desktop/Python/week6-python-selenium-task-ElminaIusifova/chromedriver')
+browser_driver = webdriver.Chrome(executable_path='./chromedriver')
 browser_driver.get('https://www.facebook.com/')
 
 browser_driver.find_element_by_name('email').send_keys('elminaapple@gmail.com')
 browser_driver.find_element_by_id('pass').send_keys('Elmina@1988!')
 browser_driver.find_element_by_css_selector('#loginbutton').click()
-
-
 
 sleep(10)
 
